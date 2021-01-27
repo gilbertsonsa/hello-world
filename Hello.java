2808db3b7db946cad7db1a2ee7516b5f7bec74db
gilbertsonsa <78098971+gilbertsonsa@users.noreply.github.com>
2021-01-27 11:47:02 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/*********************
+* Lab 1
+**********************
+* Sara Gilbertson
+* 1/27/2021
+* CMSC 255 S003
+**********************/
+
+public class Hello {
+	public static void main (String[] args) {
+    System.out.println("Sara Gilbertson");
+	System.out.println("Lab 1");
+	System.out.println("Intro to Course");
+	System.out.println("1/27/2021");
+	System.out.println("CMSC 255 S003");
+	System.out.println();
+	}
+}
